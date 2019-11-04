f8d32b0155ba471668a7a674c6bdbfee13ad7e76
Mikael <39443004+MkRodrigues@users.noreply.github.com>
2019-11-04 17:13:09 -0300

    Inserindo Dados Curso
    Inserindo conf do banco MYSQLI

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "Hello World";
+#mysqli 
+// Usado para acessar o banco de dados
+// Os dados a seguir são necessários para realizar a conexão com o Banco
+$servidor = "localhost:8000";
+$usuario = "root";
+$senha = "password";
+$database = "curso_php";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $database);
+
+if($conexao) {
+    echo "Conectado com sucesso";
+}else{
+    echo "Conexão não realizada!"; 
+}

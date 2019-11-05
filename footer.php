0e1e19400e4361a6389214a54961d82766e519cd
Mikael <39443004+MkRodrigues@users.noreply.github.com>
2019-11-05 17:00:21 -0300

    Envio

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+<!-- Fechamento div Conteudo from Header  -->
+</div>
+
+<footer>
+    <div class="container">
+        <p>©2018 DogCursos - Todos os direitos reservados</p>
+    </div>
+</footer>
+
+</body>
+</html>
